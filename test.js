34aefb1e61069588d6f91e372fc04a0b514879ca
q3rv0 <>
2022-10-26 22:10:11 -0300

    farole farole farole

--- test.js
+++ test.js
@@ -1,3 +1,2 @@
-img = new Image;
-img.src = 'https://webhook.site/dcef38f1-ba90-478d-811a-1c617943bc3c/bypassed';
+document.location.href = 'https://webhook.site/dcef38f1-ba90-478d-811a-1c617943bc3c/bypassed';
 
